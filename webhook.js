b23611b53d49c5fda327e75f68ad9898aca27ec5
Xie <Yukai.Xie@lcfuturecenter.com>
2020-12-16 20:17:33 +0800

    feat: init

---
+++ webhook.js
@@ -0,0 +1,16 @@
+let http = require('http');
+
+let server = http.createServer((req, res) => {
+    console.log(req.method, req.url);
+    if (req.method == 'post' && req.url == '/webhook') {
+        // 给github服务器发一个回应
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ ok: true }));
+    } else {
+        res.end('Not Found');
+    }
+});
+
+server.listen(4000, () => {
+    console.log('服务已经在4000端口上启动了');
+});
