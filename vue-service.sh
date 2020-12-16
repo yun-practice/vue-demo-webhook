164e643892c15be72b7d3eed8f4e7b0c1cdf186a
Xie <Yukai.Xie@lcfuturecenter.com>
2020-12-16 21:59:26 +0800

    feat: modify sh

--- vue-service.sh
+++ vue-service.sh
@@ -8,9 +8,9 @@ echo '拉取最新代码'
 git pull origin master
 echo '开始执行构建'
 # 去vue-service当前目录下找dockerfile文件
-docker build -t vue-service .
+docker build -t vue-service:1.0 .
 echo '先停止旧容器并删除旧容器'
 docker stop vue-service-container
 docker rm vue-service-container
 echo '启动新容器'
-docker container run -p 8083:8083 --name vue-service-container -d vue-service
+docker container run -p 8083:8083 --name vue-service-container -d vue-service:1.0
